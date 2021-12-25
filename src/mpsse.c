d8c54a92759f54cb6e70077702e891d3420a146c
aaron <aaron.low.au@gmail.com>
2021-12-26 00:05:54 +0800

    Adding pthread linking for linux build try 3

--- src/mpsse.c
+++ src/mpsse.c
@@ -169,7 +169,13 @@ struct mpsse_context *OpenIndex(int vid, int pid, enum modes mode, int freq,
                                 mpsse->open = 1;
 
                                 /* Give the chip a few mS to initialize */
+#ifdef APPLE
                                 usleep(SETUP_DELAY);
+#elifdef LINUX
+                                nanosleep(SETUP_DELAY * 1000);
+#else
+#error Only mac os or linux platforms supported
+#endif
 
                                 /*
                                  * Not all FTDI chips support all the commands
